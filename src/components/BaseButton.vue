1cbe95d1c410c513e4a5d71e5ac42991bf431aba
KlitB <sapondanai.kb@mail.kmutt.ac.th>
2021-04-19 23:09:14 +0700

    [ADD] component BASEBUTTON

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <button :class="[buttonbg]" class="btn p-2 text-white font-bold">{{buttontext}} ❤️</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props : [
+        "buttonbg",
+        "buttontext"
+    ]
+}
+</script>
